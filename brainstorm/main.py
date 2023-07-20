e70b09d505ced84c39a064ef4062d1a12b688389
Nate Allen <nate.allen@tripleblind.com>
2023-07-20 15:48:18 -0500

    Fixed bugs. Added features. - Works on mobile. It was never a mobile problem, it was a problem    with using another computer. - You can now skip a clarification question.

--- brainstorm/main.py
+++ brainstorm/main.py
@@ -8,14 +8,14 @@ class Consultant():
 Tavita is a consultant. She is a detail oriented, experienced, empathetic, genius solver of problems. She is meeting with a client who is facing a problem. Here is their conversation:
 
 Client: {{problem_description}}.
-{% for item in clarification_answers %}
+{% for item in clarification_answers -%}
 Tavita: {{ item.question }}
 Client: {{ item.answer }}
 {% endfor %}
-Tavita takes notes dilligently and thinks through the problem. She pauses to consider her options.
+Tavita takes notes diligently and thinks through the problem. She pauses to consider her options.
 Tavita: I can think of a few different approaches you could take. Let me collect my thoughts an document them in an actionble manner for you. I'll email them by the end of the day.
 
-Tavita writes up a report with the following summary of the client's problem, along with {{number_of_solutions}} actionable plans:
+Tavita writes up a report with the following summary of the client's problem, along with {{number_of_solutions}} actionable plans, formatted in markdown:
 """
 
     clarification_prompt_template = """Tavita is a consultant. She is a detail oriented, experienced, empathetic, genius solver of problems.
@@ -35,22 +35,30 @@ As an expert, Tavita knows that the client may not have given her all the inform
         # input_variables=["clarification_answers","problem_statement","number_of_solutions"], 
         template=solution_prompt_template, template_format="jinja2"
     )
+    fewer_tokens = False
 
     def get_clarification(self, problem_description):
         formatted_clarification_prompt = self.clarification_prompt.format(problem_description=problem_description)
         print(formatted_clarification_prompt)
-        result = self.llm(formatted_clarification_prompt)
+        if(self.fewer_tokens):
+            result = " Just one quesiton for you.\n- Ok, two."
+        else:
+            result = self.llm(formatted_clarification_prompt)
         print(result)
         return [s.strip() for s in result.split("- ")]
 
     def get_solution(self, problem_description, clarification_answers, number_of_solutions):
+        print("clarification_answers:", clarification_answers)
         formatted_solution_prompt = self.solution_prompt.format(
             problem_description=problem_description,
             clarification_answers=clarification_answers,
             number_of_solutions=number_of_solutions
         )
         print(formatted_solution_prompt)
-        result = self.llm(formatted_solution_prompt)
+        if(self.fewer_tokens):
+            result = "# Solution Report:\n\n## Problem statement: \n " + problem_description + "\n\n## Solutions: \n Just love yourself, and everything will be ok."
+        else:
+            result = self.llm(formatted_solution_prompt)
         print(result)
         return result
 
